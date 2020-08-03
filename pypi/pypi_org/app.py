fa511168d48b9efaa389e379437bac77b57176ca
dastagg:opus4 <dastagg@stagglinux.com>
2020-08-03 19:18:42 -0400

    Completed Templates chapter.

---
+++ pypi/pypi_org/app.py
@@ -0,0 +1,20 @@
+import flask
+
+app = flask.Flask(__name__)
+
+def get_latest_packages():
+    return [
+        {"name": "flask", "version": "1.2.3"},
+        {"name": "sqlalchemy", "version": "2.3.4"},
+        {"name": "passlib", "version": "3.4.5"},
+
+    ]
+
+@app.route('/')
+def index():
+    test_packages = get_latest_packages()
+    return flask.render_template('index.html', packages=test_packages)
+
+
+if __name__ == '__main__':
+    app.run()
