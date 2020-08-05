0322cee784ea60a1661088e52dac484e61e773bc
dastagg:opus4 <dastagg@stagglinux.com>
2020-08-05 07:49:40 -0400

    End of Chapter 9: Modeling data.

--- pypi/pypi_org/app.py
+++ pypi/pypi_org/app.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import flask
+import pypi_org.data.db_session as db_session
 
 folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, folder)
@@ -10,13 +11,21 @@ app = flask.Flask(__name__)
 
 def main():
     register_blueprints()
+    setup_db()
     app.run(debug=True)
+
+
+def setup_db():
+    db_file = os.path.join(
+        os.path.dirname(__file__),
+        'db',
+        'pypi.db')
+    db_session.global_init(db_file)
 
 
 def register_blueprints():
     from pypi_org.views import home_views
     app.register_blueprint(home_views.blueprint)
-
     from pypi_org.views import package_views
     app.register_blueprint(package_views.blueprint)
 
@@ -27,3 +36,4 @@ if __name__ == '__main__':
     main()
 else:
     register_blueprints()
+    setup_db()
